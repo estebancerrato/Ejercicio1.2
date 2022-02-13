ef04539f1980c791a7e055d16932efe40645b9be
estebancerrato <estebancerrato@hotmail.com>
2022-02-13 01:47:07 -0600

    Initial commit

---
+++ app/src/main/java/com/example/ejercicio12/ActivityResultado.java
@@ -0,0 +1,29 @@
+package com.example.ejercicio12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.ejercicio12.configuraciones.Personas;
+
+public class ActivityResultado extends AppCompatActivity {
+
+    TextView nombres, apellidos, edad, correo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultado);
+
+        nombres = (TextView) findViewById(R.id.lblNombres);
+        apellidos = (TextView) findViewById(R.id.lblApellidos);
+        edad = (TextView) findViewById(R.id.lblEdad);
+        correo = (TextView) findViewById(R.id.lblCorreo);
+
+        nombres.setText(getIntent().getStringExtra("putnombres"));
+        apellidos.setText(getIntent().getStringExtra("putapellidos"));
+        edad.setText(getIntent().getStringExtra("putedad"));
+        correo.setText(getIntent().getStringExtra("putcorreo"));
+    }
+}
